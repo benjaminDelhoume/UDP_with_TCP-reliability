53a0ead30f7fb2218f70bde8d052e3337caf7058
benjaminDelhoume <benjamin.delhoume@insa-lyon.fr>
2020-10-15 11:26:04 +0200

    NOK Starting working with thread

--- server.c
+++ server.c
@@ -9,6 +9,89 @@
 #include <sys/time.h>
 #include "funcs.h"
 
+static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+int window_len = 10;
+int server_udp_data;
+int seq_number = 0;
+int jeton = 10;
+struct sockaddr_in client;
+socklen_t client_size = sizeof(struct sockaddr);
+char sendList[10];
+
+void listen_ack()
+{
+  struct timeval timeout;
+  struct timeval send_time;
+  struct timeval receive_time;
+  fd_set receive_fd_set;
+  FD_ZERO(&receive_fd_set);
+  FD_SET(server_udp_data, &receive_fd_set);
+  long int estimate_RTT;
+  char ack_buffer[9];
+  int ack_msglen;
+  char ack_number[6];
+  int sequence_number_received = 0;
+  bool flag = true;
+
+  while (true)
+  {
+    while (jeton != 0)
+    {
+      int rcv = select(FD_SETSIZE, &receive_fd_set, NULL, NULL, &timeout);
+      if (rcv < 0)
+      {
+        error("Error during select");
+      }
+      else if (rcv == 0)
+      {
+        printf("Timeout, Resending\n");
+        receive_time = getTime();
+        estimate_RTT = estimateRTT(send_time, receive_time, estimate_RTT);
+        timeout.tv_usec = estimate_RTT;
+      }
+      else
+      {
+        int ack_msglen = recvfrom(server_udp_data, ack_buffer, 9, 0, (struct sockaddr *)client, &client_size);
+
+        receive_time = getTime();
+        estimate_RTT = estimateRTT(send_time, receive_time, estimate_RTT);
+        timeout.tv_usec = estimate_RTT;
+
+        printf("Estimate RTT %ld \n", (timeout.tv_usec));
+
+        if (ack_msglen < 0)
+        {
+          perror("Error receiving ACK\n");
+          return -1;
+        }
+
+        if (strncmp(ack_buffer, "ACK", 3) != 0)
+        {
+          perror("Bad ACK format\n");
+          return -1;
+        }
+
+        memset(ack_number, 0, 6);
+
+        strncpy(ack_number, ack_buffer + 3, 6);
+
+        sequence_number_received = atoi(ack_number);
+
+        if (sequence_number_received == seq_number)
+        {
+          printf("Good ACK number received : %d\n", sequence_number_received);
+          flag = false;
+        }
+        else
+        {
+          printf("Ack number received different from expecting : %d\n", sequence_number_received);
+          printf("Resending");
+        }
+      }
+    }
+  }
+}
+
 int main(int argc, char *argv[])
 {
   printf("Henlo\n");
@@ -22,6 +105,7 @@ int main(int argc, char *argv[])
   int receive_sequence_number = 1; // We're waiting for the 1st byte
   int send_sequence_number = 1;
   FILE *file;
+  pthread_t threadack;
 
   if (argc != 3)
   {
@@ -75,6 +159,7 @@ int main(int argc, char *argv[])
     char bufferBen[32];
     int n;
     socklen_t client_size = sizeof(struct sockaddr);
+    bool send_done;
 
     char SYN_ACK[16];
     sprintf(SYN_ACK, "SYN-ACK%d\n", data_port);
@@ -145,12 +230,22 @@ int main(int argc, char *argv[])
       //server_udp = socket(AF_INET, SOCK_DGRAM, 0);
       //setsockopt(server_udp, SOL_SOCKET, SO_REUSEADDR, (const void *)&optval, sizeof(int));
 
+      //Creating the listening thread
+      pthread_create(&threadack, NULL, listen_ack, NULL);
+
       // Starting here: we are connected to a client
       while (1)
       {
         memset(&buffer, 0, MAX_DGRAM_SIZE);
         printf("Waiting for new message\n");
-        recvsize = safe_recv(server_udp_data, buffer, &client, receive_sequence_number);
+
+        int msglen = recvfrom(server_udp_data, buffer, MAX_DGRAM_SIZE, 0, (struct sockaddr *)&client, &client_size);
+
+        if (msglen < 0)
+        {
+          perror("Error receiving message\n");
+          return -1;
+        }
 
         if (strcmp(buffer, "FIN\n") == 0)
         {
@@ -158,26 +253,33 @@ int main(int argc, char *argv[])
           break;
         }
 
-        if (recvsize < 0)
+        if (msglen < 0)
         {
           perror("Unable to receive message from client\n");
         }
 
-        receive_sequence_number += recvsize;
-        printf("Client-%d(%d)>>%s\n", data_port, recvsize, buffer);
+        receive_sequence_number += msglen;
+        printf("Client-%d(%d)>>%s\n", data_port, msglen, buffer);
 
         file = fopen(buffer, "r");
         if (file == NULL)
         {
           error("Problème lors de l'ouverture du fichier");
         }
-        recvsize = sendFile(server_udp_data, &client, file);
-        send_sequence_number += recvsize;
+        while (!send_done)
+        {
+          while (jeton != 0)
+          {
+            recvsize = sendFile(server_udp_data, &client, file);
+            send_sequence_number += recvsize;
+          }
+        }
         sendto(server_udp_data, "FIN", strlen("FIN"), 0, (struct sockaddr *)&client, sizeof(client));
 
         printf("Fin d'envoi du fichier \n");
         fclose(file);
       }
+      pthread_join(threadack, NULL);
       close(server_udp_data);
       exit(0);
     }
